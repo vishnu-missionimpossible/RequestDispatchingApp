977a093c035bb7a4134dfb9abf875b25d7184dff
Vishnu <67753734+vishnu-missionimpossible@users.noreply.github.com>
2023-01-17 17:43:33 +0530

    Add files via upload

---
+++ RequestDispatchingApp/src/main/java/in/ineuron/controller/FirstServlet.java
@@ -0,0 +1,33 @@
+package in.ineuron.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/test1")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h1 style='color:red; text-align:center'>This is First Servlet.</h1>");
+		
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/test2");
+		rd.forward(request, response);
+		
+		out.close();
+	}
+
+}
